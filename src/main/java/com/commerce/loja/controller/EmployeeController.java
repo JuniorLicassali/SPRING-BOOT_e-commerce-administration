dbfe2f36e7133aac8ce6d88f5de519c9e1366a5f
JuniorLicassali <licassali88@gmail.com>
2023-02-09 17:27:56 -0300

    Registration and editing of employees

--- src/main/java/com/commerce/loja/controller/EmployeeController.java
+++ src/main/java/com/commerce/loja/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.commerce.loja.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,17 +22,11 @@ public class EmployeeController {
 
 	@GetMapping("/administrativo/funcionarios/cadastrar")
 	public ModelAndView register(Employee employee) {
-		ModelAndView mv = new ModelAndView("administrativo/funcionarios/cadastro");
+		ModelAndView mv = new ModelAndView("administrative/employee/cadastro");
 		mv.addObject("employee", employee);
 		return mv;
 	}
 	
-	@GetMapping("/administrativo/funcionarios/listar")
-	public ModelAndView employeeList() {
-		ModelAndView mv = new ModelAndView("administrativo/funcionarios/lista");
-		mv.addObject("listOfEmployees", employeeRepository.findAll());
-		return mv;
-	}
 	
 	@PostMapping("/administrativo/funcionarios/salvar")
 	public ModelAndView save(@Validated Employee employee, BindingResult result) {
@@ -40,8 +37,20 @@ public class EmployeeController {
 		return register(new Employee());
 	}
 	
-	@GetMapping("/listar")
-	public String ver() {
-		return "administrative/employee/lista";
+	
+	@GetMapping("/administrativo/funcionarios/listar")
+	public ModelAndView employeeList() {
+		ModelAndView mv = new ModelAndView("administrative/employee/lista");
+		mv.addObject("listOfEmployees", employeeRepository.findAll());
+		return mv;
 	}
+	
+	
+	@GetMapping("/administrativo/funcionarios/editar/{id}")
+	public ModelAndView edit(@PathVariable("id") Long id) {
+		Optional<Employee> employee = employeeRepository.findById(id);
+		return register(employee.get());
+	}
+	
+
 }
